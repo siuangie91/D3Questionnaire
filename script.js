2496497951522170ddb90d1f3c96ed499b3af59d
Angie Siu <asiu@mscgroupny.com>
2017-05-12 17:02:41 -0400

    draw svg; include D3 lib

---
+++ script.js
@@ -0,0 +1,22 @@
+var x1 = 0,
+    y1 = 500,
+    x2 = 1500,
+    y2 = 1000;
+
+
+var svg = d3.select('#container')
+    .append('svg')
+    .attr("width", 2000)
+    .attr("height", 2000);
+
+
+
+var redraw = function() {
+    var
+};
+
+var reload = function() {
+    redraw();
+};
+
+reload();
