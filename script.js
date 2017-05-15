06395ce2eba3d45daf6da59d234da65ef3645a64
Angie Siu <asiu@mscgroupny.com>
2017-05-15 10:06:23 -0400

    skeleton code

--- script.js
+++ script.js
@@ -1,22 +1,40 @@
-var x1 = 0,
-    y1 = 500,
-    x2 = 1500,
-    y2 = 1000;
-
-
-var svg = d3.select('#container')
-    .append('svg')
-    .attr("width", 2000)
-    .attr("height", 2000);
-
-
-
-var redraw = function() {
-    var
+var getRandomInt = function() {
+    return Math.floor(Math.random() * 255);
 };
 
-var reload = function() {
-    redraw();
-};
+var svg = d3.select("svg");
 
-reload();
+function update(data, fill) {
+    d3.json(data, function(error, data) {
+        fill = fill || "steelblue";
+        // DATA JOIN
+        var circle = svg.selectAll("circle")
+            .data(data);
+
+        // Automatically UPDATE existin elems when creating new ones
+        // ENTER to create new elems as needed + automatically UPDATE existing elements
+        circle.enter().append("circle")
+            .merge(circle) // merge() merges arrays into a single array. In this case, it merges selections (our circle data) together.
+                .attr("r", 0)
+            .transition().duration(800).delay(function(d,i) { return i * 400; })
+                .attr("r", 10)
+                .attr("cx", function(d) { return +d.x; })
+                .attr("cy", function(d) { return +d.y; })
+                .style("fill", fill);
+
+        // EXIT to remove old elements.
+        circle.exit().transition().duration(800)
+            .attr("r", 0)
+            .remove();
+    });
+}
+
+setTimeout(function() {
+    console.log("Update using data1");
+    update("data1.json", "red");
+}, 1000);
+
+setTimeout(function() {
+    console.log("Update using data2");
+    update("data2.json", "green");
+}, 8000);
