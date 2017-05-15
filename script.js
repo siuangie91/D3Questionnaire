3918cc0bb73e0e417059d8381758ccf33253b126
Angie Siu <asiu@mscgroupny.com>
2017-05-15 14:44:11 -0400

    transition between datasets

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
 var svg = d3.select("svg");
 var container = d3.select("#container");
 
-function update(data) {
+function update(data, datasetNum) {
     d3.json(data, function(error, data) {
         var circle = svg.selectAll("circle")
             .data(data);
 
+        console.log("datasetNum: " + datasetNum);
+
         circle.enter().append("circle")
             .merge(circle)
+                .attr("class", "dataset" + datasetNum)
+                .attr("cx", function(d) { return +d.x; })
+                .attr("cy", function(d) { return +d.y; })
                 .attr("r", 0)
             .transition().duration(800).delay(function(d,i) { return i * 200; })
-                .attr("r", function(d) { return +d.r; })
-                .attr("cx", function(d) { return +d.x; })
-                .attr("cy", function(d) { return +d.y; });
+                .attr("r", function(d) { return +d.r; });
 
         circle.exit().transition().duration(800)
             .attr("r", 0)
@@ -24,12 +27,12 @@ function update(data) {
         heading.enter().append("span")
             .merge(heading)
                 .attr("class", "heading")
-                .style("opacity", 0)
-            .transition().duration(800).delay(function(d,i) { return i * 200; })
-                .style("opacity", 1)
                 .style("width", function(d) { return +d.r * 2 + "px"; })
                 .style("left", function(d) { return +d.x - d.r + "px";})
                 .style("top", function(d) { return +d.y - (d.r / 1.5) + "px";})
+                .style("opacity", 0)
+            .transition().duration(800).delay(function(d,i) { return i * 200; })
+                .style("opacity", 1)
             .text(function(d) { return d.heading; });
 
         heading.exit().transition().duration(800)
@@ -42,22 +45,35 @@ function update(data) {
         copy.enter().append("span")
             .merge(copy)
                 .attr("class", "copy")
-                .style("opacity", 0)
-            .transition().duration(800).delay(function(d,i) { return i * 200; })
-                .style("opacity", 1)
                 .style("width", function(d) { return +d.r * 2 - 20 + "px"; })
                 .style("left", function(d) { return +d.x - d.r + "px";})
                 .style("top", function(d) { return +d.y - (d.r / 2) + 20 + "px";})
+                .style("opacity", 0)
+            .transition().duration(800).delay(function(d,i) { return i * 200; })
+                .style("opacity", 1)
             .text(function(d) { return d.copy; });
+
+        svg.selectAll("circle.dataset" + datasetNum).on("click", function() {
+            update("data" + ++datasetNum + ".json", 2);
+        });
     });
 }
-setTimeout(function() {
-    console.log("Update using data1");
-    update("data1.json");
-}, 1000);
+
+d3.select("#start-button")
+    .on("click", function() {
+        update("data1.json", 1);
+    });
+
+//setTimeout(function() {
+//    console.log("Update using data1");
+//    update("data1.json", 1);
+//}, 1000);
 
 
-setTimeout(function() {
-    console.log("Update using data2");
-    update("data2.json");
-}, 3000);
+
+
+
+
+
+
+
