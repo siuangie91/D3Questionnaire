a44a37c6809bb19252cf2c9b8c7467cc9f54fd47
Angie Siu <asiu@mscgroupny.com>
2017-05-15 14:14:07 -0400

    position <span>s

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 var svg = d3.select("svg");
+var container = d3.select("#container");
 
 function update(data) {
     d3.json(data, function(error, data) {
@@ -16,6 +17,38 @@ function update(data) {
         circle.exit().transition().duration(800)
             .attr("r", 0)
             .remove();
+
+        var heading = container.selectAll("span.heading")
+            .data(data);
+
+        heading.enter().append("span")
+            .merge(heading)
+                .attr("class", "heading")
+                .style("opacity", 0)
+            .transition().duration(800).delay(function(d,i) { return i * 200; })
+                .style("opacity", 1)
+                .style("width", function(d) { return +d.r * 2 + "px"; })
+                .style("left", function(d) { return +d.x - d.r + "px";})
+                .style("top", function(d) { return +d.y - (d.r / 1.5) + "px";})
+            .text(function(d) { return d.heading; });
+
+        heading.exit().transition().duration(800)
+            .attr("opacity", 0)
+            .remove();
+
+        var copy = container.selectAll("span.copy")
+            .data(data);
+
+        copy.enter().append("span")
+            .merge(copy)
+                .attr("class", "copy")
+                .style("opacity", 0)
+            .transition().duration(800).delay(function(d,i) { return i * 200; })
+                .style("opacity", 1)
+                .style("width", function(d) { return +d.r * 2 - 20 + "px"; })
+                .style("left", function(d) { return +d.x - d.r + "px";})
+                .style("top", function(d) { return +d.y - (d.r / 2) + 20 + "px";})
+            .text(function(d) { return d.copy; });
     });
 }
 setTimeout(function() {
