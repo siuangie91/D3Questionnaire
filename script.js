4417a483973dc5143edb6e165945b364b5b86796
Angie Siu <asiu@mscgroupny.com>
2017-05-15 15:56:02 -0400

    group span.heading and span.copy in a div.text container

--- script.js
+++ script.js
@@ -3,11 +3,13 @@ var container = d3.select("#container");
 
 function update(data, datasetNum) {
     d3.json(data, function(error, data) {
+        // DATA JOIN circles
         var circle = svg.selectAll("circle")
             .data(data);
 
         console.log("datasetNum: " + datasetNum);
 
+        // ENTER + UPDATE circles
         circle.enter().append("circle")
             .merge(circle)
                 .attr("class", "dataset" + datasetNum)
@@ -17,46 +19,51 @@ function update(data, datasetNum) {
             .transition().duration(800).delay(function(d,i) { return i * 200; })
                 .attr("r", function(d) { return +d.r; });
 
+        // EXIT/REMOVE old circles
         circle.exit().transition().duration(800)
             .attr("r", 0)
             .remove();
 
-        var heading = container.selectAll("span.heading")
+        // DATA JOIN <div class="text">s
+        var text = container.selectAll("div.text")
             .data(data);
 
-        heading.enter().append("span")
-            .merge(heading)
-                .attr("class", "heading")
+        // ENTER + UPDATE <div class="text">s
+        text.enter().append("div")
+                .attr("class", "text")
+            .merge(text)
                 .style("width", function(d) { return +d.r * 2 + "px"; })
                 .style("left", function(d) { return +d.x - d.r + "px";})
                 .style("top", function(d) { return +d.y - (d.r / 1.5) + "px";})
                 .style("opacity", 0)
             .transition().duration(800).delay(function(d,i) { return i * 200; })
-                .style("opacity", 1)
-            .text(function(d) { return d.heading; });
+                .style("opacity", 1);
 
-        heading.exit().transition().duration(800)
+        // EXIT/REMOVE old <div class="text">s
+        text.html(null) // remove child <span class="heading">s and <span class="copy">s as well
+            .exit().transition().duration(800)
             .attr("opacity", 0)
             .remove();
 
-        var copy = container.selectAll("span.copy")
-            .data(data);
+        // Create SELECTION of all <div class="text">s, incl. newly appended ones
+        var textContainers = container.selectAll("div.text");
 
-        copy.enter().append("span")
-            .merge(copy)
-                .attr("class", "copy")
-                .style("width", function(d) { return +d.r * 2 - 20 + "px"; })
-                .style("left", function(d) { return +d.x - d.r + "px";})
-                .style("top", function(d) { return +d.y - (d.r / 2) + 20 + "px";})
-                .style("opacity", 0)
-            .transition().duration(800).delay(function(d,i) { return i * 200; })
-                .style("opacity", 1)
+        // Append <span class="heading"> to <div class="text">s
+        textContainers.append("span")
+            .attr("class", "heading")
+            .text(function(d) { return d.heading; });
+
+        // Append <span class="copy"> to <div class="text">s
+        textContainers.append("span")
+            .attr("class", "copy")
             .text(function(d) { return d.copy; });
 
+        // Attach click event listener to circles to switch to next dataset
         svg.selectAll("circle.dataset" + datasetNum + ":not(:first-of-type)")
             .on("click", function() {
                 update("data" + ++datasetNum + ".json", 2);
             });
+
     });
 }
 
@@ -65,12 +72,3 @@ d3.select("#start")
         update("data1.json", 1);
         this.remove();
     });
-
-
-
-
-
-
-
-
-
