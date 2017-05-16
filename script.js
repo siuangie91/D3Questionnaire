a9527d63b09c9e5b2291d34cb69df75b97b34434
Angie Siu <asiu@mscgroupny.com>
2017-05-16 10:20:17 -0400

    restructure code to have <div class="group">s to contain <div class="circle">s and <span>s; no SVGs

--- script.js
+++ script.js
@@ -11,41 +11,22 @@ d3.json("datafiles.json", function(error, data) {
     console.log("lastFileNum: "+ lastFileNum);
 });
 
-var svg = d3.select("svg");
+//var svg = d3.select("svg");
 var container = d3.select("#container");
 
-function update(data, datasetNum) {
+var update = function(data, datasetNum) {
     d3.json(data, function(error, data) {
-        // DATA JOIN circles
-        var circle = svg.selectAll("circle")
+        // DATA JOIN <div class="group">s
+        var group = container.selectAll("div.group")
             .data(data);
 
-        console.log("datasetNum: " + datasetNum);
-
-        // ENTER + UPDATE circles
-        circle.enter().append("circle")
-            .merge(circle)
-                .attr("class", "dataset" + datasetNum)
-                .attr("cx", function(d) { return +d.x; })
-                .attr("cy", function(d) { return +d.y; })
-                .attr("r", 0)
-            .transition().duration(800).delay(function(d,i) { return i * 200; })
-                .attr("r", function(d) { return +d.r; });
-
-        // EXIT/REMOVE old circles
-        circle.exit().transition().duration(800)
-            .attr("r", 0)
-            .remove();
-
-        // DATA JOIN <div class="text">s
-        var text = container.selectAll("div.text")
-            .data(data);
-
-        // ENTER + UPDATE <div class="text">s
-        text.enter().append("div")
-                .attr("class", "text")
-            .merge(text)
+        // ENTER + UPDATE groups
+        group.enter().append("div")
+                .classed("group", true)
+            .merge(group)
+                .classed("dataset" + datasetNum, true)
                 .style("width", function(d) { return +d.r * 2 + "px"; })
+                .style("height", function(d) { return +d.r * 2 + "px"; })
                 .style("left", function(d) { return +d.x - d.r + "px";})
                 .style("top", function(d) { return +d.y - (d.r / 1.5) + "px";})
                 .style("opacity", 0)
@@ -53,40 +34,43 @@ function update(data, datasetNum) {
                 .style("opacity", 1);
 
         // EXIT/REMOVE old <div class="text">s
-        text.html(null) // remove child <span class="heading">s and <span class="copy">s as well
+        group.html(null) // remove child <span class="heading">s and <span class="copy">s as well
             .exit().transition().duration(800)
             .attr("opacity", 0)
             .remove();
 
-        // Create SELECTION of all <div class="text">s, incl. newly appended ones
-        var textContainers = container.selectAll("div.text");
+        // Recreate SELECTION of all <div class="group">s, incl. newly appended ones
+        var groupContainers = container.selectAll("div.group");
 
-        // Append <span class="heading"> to <div class="text">s
-        textContainers.append("span")
-            .attr("class", "heading")
+        // Append <div class="circle"> to <div class="group">s
+        groupContainers.append("div")
+            .classed("circle", true);
+
+        // Append <span class="heading"> to <div class="group">s
+        groupContainers.append("span")
+            .classed("heading", true)
             .text(function(d) { return d.heading; });
 
-        // Append <span class="copy"> to <div class="text">s
-        textContainers.append("span")
-            .attr("class", "copy")
+        // Append <span class="copy"> to <div class="group">s
+        groupContainers.append("span")
+            .classed("copy", true)
             .text(function(d) { return d.copy; });
 
         // Attach click event listener to circles to switch to next dataset
-        svg.selectAll("circle.dataset" + datasetNum + ":not(:first-of-type)")
+        container.selectAll(".group.dataset" + datasetNum + ":not(:first-of-type)")
             .on("click", function() {
                 update("data" + ++datasetNum + ".json", datasetNum++);
             });
 
         // If on last dataset, clicking button reloads page.
-        if(svg.selectAll("circle.dataset" + lastFileNum).size()) {
-            svg.selectAll("circle.dataset" + lastFileNum)
+        if(container.selectAll(".group.dataset" + lastFileNum).size()) {
+            container.selectAll(".group.dataset" + lastFileNum)
                 .on("click", function() {
                     window.location.reload();
                 });
         }
-
     });
-}
+};
 
 d3.select("#start")
     .on("click", function() {
